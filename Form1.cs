adac80cafa0f1bc11551722b2aa037aaa81f3621
재서 김 <23010114@suwon.ac.kr>
2026-05-23 20:37:59 +0900

    develop: services폴더 삭제 및 Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.Windows.Forms;
-using DateManager.services; // 서비스 네임스페이스를 사용합니다.
 
 namespace DateManager
 {
     public partial class Form1 : Form
     {
-        // develop 브랜치의 명명 규칙을 따르는 것이 좋습니다.
-        private CatalogFrameController? catalogFrameController;
-
         public Form1()
         {
+            // UI 컴포넌트를 초기화합니다. (디자인 창의 요소를 불러옴)
             InitializeComponent();
 
-            // 컨트롤러가 UI 컨트롤들을 찾아서 자동으로 연결합니다.
-            catalogFrameController = CatalogFrameController.TryAttach(this);
+            // TODO: 새로운 팀원이 만든 기능(컨트롤러나 매니저 등)을 이 아래에 연결하세요.
 
-            if (catalogFrameController == null)
-            {
-                MessageBox.Show("UI 컨트롤 연결 실패! 디자인 창에서 버튼/라벨 이름을 확인하세요.");
-            }
         }
     }
 }
